8c9b11fed3e35a1b9e5887fc5c41fbc85e53cd0c
Ashik Api <ashikapi65@gmail.com>
2026-02-16 14:46:20 +0600

    add comments

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+// Import the Express module
 const express = require('express');
+
+
+// Create an instance of Express
  const app = express();
+
+ // Route for the root URL ('/')
+// Sends a simple "Hello World" message
   app.get('/', (req , res) => {
     res.send('Hello World');
   });
 
+
+  // Route to get your name
+// When the user visits '/api/myname', it responds with your name
   app.get('/api/myname', (req , res) => {
     res.send('My name is Ashik');
   });
 
+
+  // Route to get information about some people
+// Responds with a JSON array containing id, name, and age
   app.get('/api/person/info', (req, res) => {
     res.json([
         {id: 1, name: 'Ashik', age: 25},
@@ -16,6 +29,9 @@ const express = require('express');
     ]);
   });
 
+
+// Start the server on port 5000
+// Logs a message to the console when the server is running
 
   app.listen(5000, () => {
     console.log('Server is running on port 5000');
